d7aec201dc0700193f80886005dc6dfffbf1b411
fl86kyvo <fl86kyvo@URZ-VWIT0-M011.adds.tu-freiberg.de>
2025-03-11 13:01:44 +0100

    Neue Lösungen -Studienverlauf mit Publish Modul -Comboboxinhalte aus Generischer Suche

---
+++ WEB-INF/conf/cs/sys/reporting/customSettings/1readmeFGL.txt
@@ -0,0 +1,4 @@
+FGL 20250311:
+Beispiele für Suchkonfigurationsdateien, die in dem CustomSettings - Step eines 
+Auftrags eingebunden werden können 
+und die den Inhalt einer ComboBox / DropDown Liste füllen können.
---
+++ WEB-INF/fgl/conf/202312/job/job_cm_stu_bs_Exma/Lösungsansatz - PublishDataRequestStep.sql
@@ -0,0 +1,39 @@
+set search_path=hisinone;
+ 
+select * from student
+where registrationnumber in (52612);
+
+select * from degree_program
+Limit 100;
+
+-- Studienverlauf eines Studierenden
+select degree_program_id, *
+from degree_program, degree_program_progress 
+where degree_program_id = degree_program.id 
+and degree_program_id in 
+(select id from degree_program where person_id in 
+(select person_id from student where registrationnumber in (52612))) 
+order by startdate, studynumber, subjectnumber
+
+-- Course of Study , wg. der Studiengänge
+set search_path=hisinone;
+select * from course_of_study
+where id in
+(select course_of_study_id 
+	from degree_program, degree_program_progress 
+	where degree_program_id = degree_program.id 
+	and degree_program_id in 
+		(select id from degree_program where person_id in 
+			(select person_id from student where registrationnumber in (52612))
+		) 
+		order by startdate, studynumber, subjectnumber
+)
+
+-- Aggregation der Daten aus dem Studienverlauf eines Studierenden für eine degree_program_id 
+set search_path=hisinone;
+select min(startdate), max(enddate)
+from degree_program, degree_program_progress 
+where degree_program_id = degree_program.id 
+and degree_program_id in 
+(764) 
+group by degree_program_id
