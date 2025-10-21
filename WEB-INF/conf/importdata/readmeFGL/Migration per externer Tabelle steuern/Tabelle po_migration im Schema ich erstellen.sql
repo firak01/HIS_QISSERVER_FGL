491faeaca0ee8faa10e6d9b04b05c08e78017b43
fl86kyvo <fl86kyvo@URZ-VWIT0-M011.adds.tu-freiberg.de>
2025-10-21 11:36:20 +0200

    1

--- WEB-INF/conf/importdata/readmeFGL/Migration per externer Tabelle steuern/Tabelle po_migration im Schema ich erstellen.sql
+++ WEB-INF/conf/importdata/readmeFGL/Migration per externer Tabelle steuern/Tabelle po_migration im Schema ich erstellen.sql
@@ -12,7 +12,6 @@ CREATE TABLE ich.po_migration (
 -- Optional: Die Sequenz mit der Tabelle verknüpfen (empfohlen)
 ALTER SEQUENCE ich.po_migration_reihenfolge_seq OWNED BY ich.po_migration.reihenfolge;
 
-
 ----------------------------------
 --- TABELLE FÜLLEN
 INSERT INTO ich.po_migration (
@@ -20,4 +19,30 @@ INSERT INTO ich.po_migration (
 ) VALUES (
     '88-AB8---2024'
 );
+
+
+INSERT INTO ich.po_migration (
+    tubaf_po
+) VALUES (
+    '88-039---2022'
+);
+
+--- Sortierte ansehen, die erledigten nach vorne
+select *, CASE migriert
+			WHEN true THEN 1
+			ELSE 0
+		 END AS sortierer
+from ich.po_migration
+order by sortierer DESC, reihenfolge ASC;
+
+--- MIGRIERT STATUS ÄNDERN, alle wieder zurueck
+update ich.po_migration
+SET migriert = false
+WHERE reihenfolge >= 1;
+
+--- REIHENFOLGE GGFS. ÄNDERN, durch Dreickstauschtausch möglich wenn man keine Lücken in der Reihenfolge haben will.
+update ich.po_migration
+SET reihenfolge = 3 
+WHERE reihenfolge = 1;
+
 
